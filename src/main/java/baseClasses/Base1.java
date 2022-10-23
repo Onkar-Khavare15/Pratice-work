ef8b16b06ee1cc90a5f80b2cea22bbcde1b050bf
unknown <onkarkhavare90@gmail.com>
2022-10-23 14:26:59 +0530

    First commit

---
+++ src/main/java/baseClasses/Base1.java
@@ -0,0 +1,76 @@
+package baseClasses;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Base1 {
+	
+static WebDriver driver;
+
+static ExtentHtmlReporter htmlReporter;
+static ExtentReports reports;
+static ExtentTest test;
+	
+	//Initializations
+	public static WebDriver getDriver(String browser) {
+		
+		if(driver == null) {
+			
+			if(browser.equals("chrome")) {
+				
+				WebDriverManager.chromedriver().setup();
+				
+		//		System.setProperty("webdriver.chrome.driver", "D:\\Velocity\\Batch 18th June 2022\\chromedriver.exe");
+				driver = new ChromeDriver();
+			}else if(browser.equals("firefox")) {
+				
+				WebDriverManager.firefoxdriver().setup();
+				
+			//	System.setProperty("webdriver.gecko.driver", "D:\\Velocity\\Batch 18th June 2022\\geckodriver.exe");
+				driver = new FirefoxDriver();
+			}
+
+				driver.get("https://www.flipkart.com/");
+
+			
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		}
+		
+		return driver;
+	}
+	
+	public static void unloadDriver() {
+		driver = null;
+	}
+	public static ExtentHtmlReporter getHtmlReporter()	{
+		
+		if(htmlReporter == null)	{
+			htmlReporter = new ExtentHtmlReporter("ExtentReports.html");
+		}
+		return htmlReporter;
+	}
+	
+	public static ExtentReports getReporters()	{
+		if (reports==null)	{
+			reports = new ExtentReports();
+			reports.attachReporter(htmlReporter);
+		}	
+		return reports;
+	}
+	
+	public static ExtentTest getExtentTest(String testName) {
+		
+		return test = reports.createTest(testName);
+		
+	}
+}
---
+++ src/test/java/testClasses/VerifyUserCanGetLowestPriceProduct.java
@@ -0,0 +1,96 @@
+package testClasses;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+import UtilClasses.Util1;
+import baseClasses.Base1;
+import pomClasses.HomePage;
+
+public class VerifyUserCanGetLowestPriceProduct {
+	
+ExtentHtmlReporter htmlReporter;
+ExtentReports reports;
+ExtentTest test;
+
+WebDriver driver;
+	
+	HomePage hp;
+	
+	@BeforeClass
+	@Parameters("browser")
+	public void beforeClass(String browser) {
+		htmlReporter = Base1.getHtmlReporter();
+		reports = Base1.getReporters();
+		test = Base1.getExtentTest("VerifyUserCanGetLowestPriceProduct");
+		
+		driver = Base1.getDriver(browser);
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		hp = new HomePage(driver);
+	}
+	
+	@Test()
+	public void VerifyUserGetLowestPriceProduct() {
+		hp.searchProduct();	
+		
+		for(int i=1; i<=5; i++) {
+			
+			if(i != 1) {
+				hp.switchPage(i);
+			}
+			
+			Assert.assertNotEquals(hp.getLowsetPrice(), "");
+		}
+		
+		
+		
+	}
+	
+	
+	@AfterMethod
+	public void afterMethod(ITestResult result) throws IOException {
+		if(result.getStatus() == ITestResult.SUCCESS) {
+			test.log(Status.PASS, result.getName() +" is Passed");
+		}
+		
+		
+		
+		else if(result.getStatus() == ITestResult.FAILURE)
+		{
+			String path = Util1.getScreenShotPath(driver, result.getName());
+			
+			test.log(Status.FAIL, result.getName() +" is Failed", MediaEntityBuilder.createScreenCaptureFromPath(path).build());
+		}
+		
+		else if(result.getStatus() == ITestResult.SKIP){
+			
+			test.log(Status.FAIL, result.getName() +" is Skipped");
+		}
+	
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		reports.flush();
+		Base1.unloadDriver();
+	}
+
+}
